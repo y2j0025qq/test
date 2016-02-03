cb1d1947327e4e0ad627aa95afe9ec8ac5bfecb0
y2j0025qq <y2j0025qq@gmail.com>
2016-02-03 17:19:12 +0800

    aaa

---
+++ mountSharedFile2.ps1
@@ -0,0 +1,3 @@
+cmdkey /add:mysharedfile01.file.core.windows.net /user:mysharedfile01 /pass:KQ12WtoGNrA6EOJSqankDIdrS3dfkVHmfb2lgWI9kyowyjL+adsa+XyoAlydW/IK4UJxLH/jISLWDoCJKb+tqg==
+net use z: \\mysharedfile01.file.core.windows.net\aaa
+msiexec /i "Z:\WinRAR.msi" /qn
